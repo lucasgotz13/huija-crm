be0af9cbd1492f2d380f6b389a3157f95854de21
Lucas Gotz <lucasgotz13@gmail.com>
2025-02-17 05:54:14 -0300

    fix: Parametro faltante en el componente EncargadoItem en EncargadoGroupPage.tsx

--- app/components/EncargadoGroupPage.tsx
+++ app/components/EncargadoGroupPage.tsx
@@ -78,6 +78,7 @@ export default function EncargadoGroupPage({
                     key={propiedad.id}
                     item={propiedad}
                     SubmitSwitch={SubmitSwitch}
+                    isGrouped={true}
                 />
             ))}
         </div>
