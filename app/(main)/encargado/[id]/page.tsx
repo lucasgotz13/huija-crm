6eb1871c6f7f3249972d4c2d3df2d83205ac6aed
Lucas Gotz <lucasgotz13@gmail.com>
2025-02-26 04:44:04 -0300

    feat: Botón para volver a la página de Encargado agregado a propiedades de grupo

--- app/(main)/encargado/[id]/page.tsx
+++ app/(main)/encargado/[id]/page.tsx
@@ -1,5 +1,8 @@
 import EncargadoGroupPage from "@/app/components/EncargadoGroupPage";
+import { Button } from "@/components/ui/button";
 import { createClient } from "@/utils/supabase/server";
+import { MoveLeft } from "lucide-react";
+import Link from "next/link";
 
 export default async function GrupoPage({
     params,
@@ -19,6 +22,12 @@ export default async function GrupoPage({
     const sortedPropiedades = propiedades.sort((a, b) => a.id - b.id);
     return (
         <div>
+            <Link href={"/encargado"}>
+                <Button variant="outline">
+                    <MoveLeft className="h-6 w-6 md:mr-2" />
+                    <span className="hidden md:inline">Volver</span>
+                </Button>
+            </Link>
             <h1 className="mb-4 capitalize text-2xl font-bold ">{params.id}</h1>
             <EncargadoGroupPage
                 propiedades={sortedPropiedades}
